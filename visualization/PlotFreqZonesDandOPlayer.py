cffffb25d8bef3e19248b2fa45fdb54ed83af31b
Ryan Davis <rd11490@gmail.com>
2017-12-26 22:43:14 -0500

    Converting a few stragglers to SQL source data

--- visualization/PlotFreqZonesDandOPlayer.py
+++ visualization/PlotFreqZonesDandOPlayer.py
@@ -1,8 +1,10 @@
 import matplotlib.colors as colors
-import pandas as pd
-import drawCourt
 import matplotlib.pyplot as plt
+import pandas as pd
+
+import MySQLConnector
 import ShotZones
+import drawCourt
 
 shotZones = ShotZones.buildShotZones()
 valueToCreate = "Frequency"
@@ -11,8 +13,23 @@ valueToCreateLeague = "Frequency_league"
 player = "Andrew Wiggins"
 valueForPlotting = "Frequency Diff"
 
-shotZonesD = pd.read_csv("data/ePPS/D1718ByPlayerZoned.csv")
-shotZonesO = pd.read_csv("data/ePPS/O1718ByPlayerZoned.csv")
+sql = MySQLConnector.MySQLConnector()
+season = "2017-18"
+shotCutoff = 250
+
+o_query = "SELECT * FROM (select * from nba.offense_expected_points_by_player_zoned " \
+          "WHERE season = '{0}' and attempts > {1} ) a " \
+          "left join  (SELECT primaryKey, playerName FROM nba.roster_player WHERE season = '{0}') b " \
+          "on (a.id = b.primaryKey)".format(season, shotCutoff)
+
+d_query = "SELECT * FROM (select * from nba.defense_expected_points_by_player_zoned " \
+          "WHERE season = '{0}' and attempts > {1} ) a " \
+          "left join  (SELECT primaryKey, playerName FROM nba.roster_player " \
+          "WHERE season = '{0}') b " \
+          "on (a.id = b.primaryKey)".format(season, shotCutoff)
+
+shotZonesO = sql.runQuery(o_query)
+shotZonesD = sql.runQuery(d_query)
 
 shotZonesD = shotZonesD[shotZonesD["playerName"] == player]
 shotZonesO = shotZonesO[shotZonesO["playerName"] == player]
@@ -20,20 +37,20 @@ shotZonesO = shotZonesO[shotZonesO["playerName"] == player]
 shotZonesDLeague = pd.read_csv("data/LeagueDShotChartAttempts.csv")
 shotZonesOLeague = pd.read_csv("data/LeagueOShotChartAttempts.csv")
 
-shotZonesD[valueToCreate] = 100*shotZonesD["attempts"]/shotZonesD["attempts"].sum()
-shotZonesO[valueToCreate] = 100*shotZonesO["attempts"]/shotZonesO["attempts"].sum()
+shotZonesD[valueToCreate] = 100 * shotZonesD["attempts"] / shotZonesD["attempts"].sum()
+shotZonesO[valueToCreate] = 100 * shotZonesO["attempts"] / shotZonesO["attempts"].sum()
 
-shotZonesDLeague[valueToCreate] = 100*shotZonesDLeague["attempts"]/shotZonesDLeague["attempts"].sum()
-shotZonesOLeague[valueToCreate] = 100*shotZonesOLeague["attempts"]/shotZonesOLeague["attempts"].sum()
+shotZonesDLeague[valueToCreate] = 100 * shotZonesDLeague["attempts"] / shotZonesDLeague["attempts"].sum()
+shotZonesOLeague[valueToCreate] = 100 * shotZonesOLeague["attempts"] / shotZonesOLeague["attempts"].sum()
 
-shotZonesDJoined = shotZonesD.merge(shotZonesDLeague[["bin", valueToCreate]], on="bin", suffixes=["_player","_league"])
+shotZonesDJoined = shotZonesD.merge(shotZonesDLeague[["bin", valueToCreate]], on="bin", suffixes=["_player", "_league"])
 shotZonesDJoined[valueForPlotting] = shotZonesDJoined[valueToCreatePlayer] - shotZonesDJoined[valueToCreateLeague]
 
-shotZonesOJoined = shotZonesO.merge(shotZonesOLeague[["bin", valueToCreate]], on="bin", suffixes=["_player","_league"])
+shotZonesOJoined = shotZonesO.merge(shotZonesOLeague[["bin", valueToCreate]], on="bin", suffixes=["_player", "_league"])
 shotZonesOJoined[valueForPlotting] = shotZonesOJoined[valueToCreatePlayer] - shotZonesOJoined[valueToCreateLeague]
 
 maxVal = 4  # max(max(shotZonesO[valueForPlotting]), max(shotZonesD[valueForPlotting]))
-minVal = -4 # min(min(shotZonesO[valueForPlotting]), min(shotZonesD[valueForPlotting]))
+minVal = -4  # min(min(shotZonesO[valueForPlotting]), min(shotZonesD[valueForPlotting]))
 
 norm = colors.Normalize(vmin=minVal, vmax=maxVal)
 
@@ -54,7 +71,6 @@ for name in teamNames:
 
     # OFFENSE
     plt.rcParams["figure.figsize"] = [16, 6]
-    # plt.colorbar( mappable=sm, ticks=[minVal, (maxVal + minVal) / 2, maxVal])
 
     plt.subplot(1, 2, 1)
     plt.xlim(-250, 250)
@@ -75,19 +91,21 @@ for name in teamNames:
         xAvg = sum(zoneLocs["X"]) / len(zoneLocs["X"])
         yAvg = sum(zoneLocs["Y"]) / len(zoneLocs["Y"])
 
-        if (bin == "Right27FT" or bin == "Left27FT"):
-            yAvg = yAvg + 20
-        elif (bin == "Right23FT" or bin == "Left23FT"):
-            yAvg = yAvg + 10
-        elif (bin == "RightLong3" or bin == "LeftLong3"):
-            yAvg = yAvg - 50
+        if bin == "Right27FT" or bin == "Left27FT":
+            yAvg += 20
+        elif bin == "Right23FT" or bin == "Left23FT":
+            yAvg += 10
+        elif bin == "RightLong3" or bin == "LeftLong3":
+            yAvg -= 50
 
-        if (bin == "RightCorner" or bin == "LeftCorner" or "LeftBaseline23FT" or "RightBaseline23FT"):
-            txt = "{0}/{1} \n Player \n {2:.2f}%  \n League \n {3:.2f}%".format(row["made"], row["attempts"], row[valueToCreatePlayer],
-                                                                  row[valueToCreateLeague])
+        if bin == "RightCorner" or bin == "LeftCorner" or "LeftBaseline23FT" or "RightBaseline23FT":
+            txt = "{0}/{1} \n W/Player \n {2:.2f}%  \n League Avg \n {3:.2f}%".format(row["made"], row["attempts"],
+                                                                                      row[valueToCreatePlayer],
+                                                                                      row[valueToCreateLeague])
         else:
-            txt = "{0}/{1} \n Player {2:.2f}% \n League {3:.2f}%".format(row["made"], row["attempts"], row[valueToCreatePlayer],
-                                                                  row[valueToCreateLeague])
+            txt = "{0}/{1} \n W/Player {2:.2f}% \n League Avg {3:.2f}%".format(row["made"], row["attempts"],
+                                                                               row[valueToCreatePlayer],
+                                                                               row[valueToCreateLeague])
 
         plt.text(xAvg, yAvg, txt, fontdict=font)
         plt.scatter(x=zoneLocs["X"], y=zoneLocs["Y"], color=cls(norm(plotVal)), marker=".")
@@ -115,19 +133,21 @@ for name in teamNames:
         xAvg = sum(zoneLocs["X"]) / len(zoneLocs["X"])
         yAvg = sum(zoneLocs["Y"]) / len(zoneLocs["Y"])
 
-        if (bin == "Right27FT" or bin == "Left27FT"):
-            yAvg = yAvg + 20
-        elif (bin == "Right23FT" or bin == "Left23FT"):
-            yAvg = yAvg + 10
-        elif (bin == "RightLong3" or bin == "LeftLong3"):
-            yAvg = yAvg - 50
+        if bin == "Right27FT" or bin == "Left27FT":
+            yAvg += 20
+        elif bin == "Right23FT" or bin == "Left23FT":
+            yAvg += 10
+        elif bin == "RightLong3" or bin == "LeftLong3":
+            yAvg -= 50
 
-        if (bin == "RightCorner" or bin == "LeftCorner" or "LeftBaseline23FT" or "RightBaseline23FT"):
-            txt = "{0}/{1} \n Player \n {2:.2f}%  \n League \n {3:.2f}%".format(row["made"], row["attempts"], row[valueToCreatePlayer],
-                                                                  row[valueToCreateLeague])
+        if bin == "RightCorner" or bin == "LeftCorner" or "LeftBaseline23FT" or "RightBaseline23FT":
+            txt = "{0}/{1} \n W/Player \n {2:.2f}%  \n League Avg \n {3:.2f}%".format(row["made"], row["attempts"],
+                                                                                      row[valueToCreatePlayer],
+                                                                                      row[valueToCreateLeague])
         else:
-            txt = "{0}/{1} \n Player {2:.2f}% \n League {3:.2f}%".format(row["made"], row["attempts"], row[valueToCreatePlayer],
-                                                                  row[valueToCreateLeague])
+            txt = "{0}/{1} \n W/Player {2:.2f}% \n League Avg {3:.2f}%".format(row["made"], row["attempts"],
+                                                                               row[valueToCreatePlayer],
+                                                                               row[valueToCreateLeague])
 
         plt.text(xAvg, yAvg, txt, fontdict=font)
         plt.scatter(x=zoneLocs["X"], y=zoneLocs["Y"], color=cls(norm(plotVal)), marker=".")
@@ -140,4 +160,4 @@ for name in teamNames:
     plt.savefig("plots/PlayerShotFreqChart/{}".format(name), dpi=900, figsize=(14, 6))
     plt.close()
 
-    #plt.show()
+    # plt.show()
