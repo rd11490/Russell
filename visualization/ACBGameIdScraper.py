70f297748b42717b8f961bae1628e52fc034e238
Ryan Davis <rd11490@gmail.com>
2018-01-13 11:18:49 -0500

    updated ACB scraper

--- visualization/ACBGameIdScraper.py
+++ visualization/ACBGameIdScraper.py
@@ -28,24 +28,24 @@ day = 1
 sql = MySQLConnector.MySQLConnector()
 sql.drop_table(MySqlDatabases.ACBDatabase.raw_game_json, MySqlDatabases.ACBDatabase.NAME)
 while active:
-    frame = pd.DataFrame()
     print(day)
     r = http.request('GET', game_index_page(day))
     soup = bs4.BeautifulSoup(r.data, 'html')
     f = soup.find_all("a")
-    links = [extract_id(l["href"]) for l in f if "partido.php?c=" in l["href"]]
+    links = set([extract_id(l["href"]) for l in f if "partido.php?c=" in l["href"]])
     if len(links) < 1:
         active = False
     else:
+        frame = pd.DataFrame()
         for l in links:
             print(l)
             r = http.request('GET', game_stat_link(l))
             json = r.data.decode()
             frame = frame.append(pd.Series([day, l, json]), ignore_index=True)
+        frame.columns = ['day', 'id', 'json']
+        types = {'day': INTEGER,
+                 'id': sqlalchemy.types.NVARCHAR(length=255),
+                 'json': LONGTEXT}
+        sql.write(frame, MySqlDatabases.ACBDatabase.raw_game_json, MySqlDatabases.ACBDatabase.NAME, dtype=types)
         day += 1
 
-    frame.columns = ['day', 'id', 'json']
-    types = {'day': INTEGER,
-             'id': sqlalchemy.types.NVARCHAR(length=255),
-             'json': LONGTEXT}
-    sql.write(frame, MySqlDatabases.ACBDatabase.raw_game_json, MySqlDatabases.ACBDatabase.NAME, dtype=types)
